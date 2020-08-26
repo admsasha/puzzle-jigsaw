8525b96b7736ce496ebb6c5cf78a8dec777abe9d
sasha <dik@inbox.ru>
2020-08-26 10:54:03 +0900

    set translator for default widget's text

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QTime>
 #include <QDir>
 #include <QStandardPaths>
+#include <QLibraryInfo>
 
 #include "config_puzzle.h"
 #include "MainWindow.h"
@@ -31,6 +32,11 @@ int main(int argc, char *argv[]) {
 
     translator.load(QString(PATH_USERDATA)+QString("/langs/puzzle-jigsaw_") + locale);
     app.installTranslator(&translator);
+
+    //set translator for default widget's text (for example: QMessageBox's buttons)
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_"+locale,QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
 
 
     MainWindow w;
