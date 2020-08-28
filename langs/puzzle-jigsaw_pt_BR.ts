455984e43c79f876948a6ecd824ad4d249dfa70c
Fabio A. De Muzio Tobich <fabiomt@gmail.com>
2020-08-27 21:41:33 -0300

    adding pt_BR translation

---
+++ langs/puzzle-jigsaw_pt_BR.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>FormAbout</name>
+    <message>
+        <location filename="../FormAbout.ui" line="31"/>
+        <source>Puzzle jigsaw</source>
+        <translation>Puzzle Jigsaw</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="134"/>
+        <source>License: GPLv3+</source>
+        <translation>Licença: GPLv3+</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="211"/>
+        <source>Visit web site</source>
+        <translation>Visite o site</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="229"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="15"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="18"/>
+        <source>Version:</source>
+        <translation>Versão:</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="19"/>
+        <source>Date build:</source>
+        <translation>Data de construção:</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="20"/>
+        <source>All rights reserved.</source>
+        <translation>Todos os direitos reservados</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="23"/>
+        <source>Designers:</source>
+        <translation>Designers:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="44"/>
+        <source>Puzzle jigsaw v%1</source>
+        <translation>Puzzle Jigsaw v%1</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="78"/>
+        <source>Open file</source>
+        <translation>Abrir arquivo</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="85"/>
+        <source>Save puzzle to file</source>
+        <translation>Salvar quebra-cabeça em arquivo</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="92"/>
+        <source>Load puzzle from file</source>
+        <translation>Abrir quebra-cabeça do arquivo</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="99"/>
+        <source>Alignment puzzle</source>
+        <translation>Embaralhar quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="106"/>
+        <source>Show/hide preview</source>
+        <translation>Mostar/ocultar pré-visualização</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="113"/>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="120"/>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="132"/>
+        <source>Choose an image
+ for the puzzle</source>
+        <translation>Escolha uma imagem
+ para o quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="144"/>
+        <source>Puzzle [%1]</source>
+        <translation>Quebra-cabeça [%1]</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="363"/>
+        <source>You win!</source>
+        <translation>Você venceu!</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="593"/>
+        <source>open images</source>
+        <translation>abrir imagens</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="593"/>
+        <source>Images (*.png *.xpm *.jpg *.jpeg *.tiff *.webp *.bmp)</source>
+        <translation>Imagens (*.png *.xpm *.jpg *.jpeg *.tiff *.webp *.bmp)</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="645"/>
+        <source>save puzzle</source>
+        <translation>salvar quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="645"/>
+        <location filename="../MainWindow.cpp" line="681"/>
+        <source>*.puzzle</source>
+        <translation>* quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="681"/>
+        <source>load puzzle</source>
+        <translation>carregar quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="686"/>
+        <source>puzzle</source>
+        <translation>quebra-cabeça</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="686"/>
+        <source>Can&apos;t open the file %1</source>
+        <translation>Não foi possível abrir o arquivo %1</translation>
+    </message>
+</context>
+</TS>
